984a64ddefe07f801066872b8904eaa81aa6fb5a
Denison Kolling <denisonkolling@gmail.com>
2023-08-10 08:14:32 -0300

    add api request catch error

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,32 +4,37 @@ import Input from '../components/Input';
 import ItemRepo from '../components/ItemRepo';
 import { Container } from './styles';
 import { useState } from 'react';
-import {api} from '../services/api'
+import { api } from '../services/api';
 
 function App() {
-
-
 	const [currentRepo, setCurrentRepo] = useState('');
-	const [repos, setRepos] = useState([])
+	const [repos, setRepos] = useState([]);
 
-	const handleSearchRepo = async () => {
+	async function handleSearchRepo() {
+		try {
+			const { data } = await api.get(`repos/${currentRepo}`);
 
-		const {data} = await api.get(`repos/${currentRepo}`)
-
-		if(data.id){
-			setRepos(prev => [...prev, data])
-			setCurrentRepo('')
-			return
+			if (data.id) {
+				setRepos((prev) => [...prev, data]);
+				setCurrentRepo('');
+				return;
+			}
+		} catch (error) {
+			alert(error);
 		}
 	}
 
 	return (
 		<Container>
-			<img src={logo} width={72} height={72} alt='git hub logo'/>
-			<Input value={currentRepo} onChange={(e)=>setCurrentRepo(e.target.value)}/>
-			<Button onClick={handleSearchRepo}/>
-			{repos.map(repo => <ItemRepo key={repo.id} repo={repo} /> )}
-			
+			<img src={logo} width={72} height={72} alt="git hub logo" />
+			<Input
+				value={currentRepo}
+				onChange={(e) => setCurrentRepo(e.target.value)}
+			/>
+			<Button onClick={handleSearchRepo} />
+			{repos.map((repo) => (
+				<ItemRepo key={repo.id} repo={repo} />
+			))}
 		</Container>
 	);
 }
